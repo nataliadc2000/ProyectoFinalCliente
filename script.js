d620b21521d658e4a4cdd854d2bace28617f264f
Natalia Domínguez <ndcarbajosa@gmail.com>
2023-11-14 20:28:29 +0100

    creacion de la parte visual de la parte del proyecto de cliente

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("btnResgitrar").addEventListener("click", function () {
+    const data = {
+      email: document.getElementById("email").value,
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+      name: {
+        firstname: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value
+      },
+      address: {
+        city: document.getElementById("city").value,
+        street: document.getElementById("street").value,
+        number: document.getElementById("number").value,
+        zipcode: document.getElementById("zipcode").value,
+        geolocation: {
+          lat: document.getElementById("lat").value,
+          long: document.getElementById("long").value
+        }
+      },
+      phone: document.getElementById("phone").value
+    };
 
-
-
-//creacion de la funcion en la base de datos de los usuarios que se van a introducir en la api
-document.getElementById("btnResgitrar").addEventListener("click", function () {
-
-  const data = {
-    email: document.getElementById("email").value,
-    username: document.getElementById("username").value,
-    password: document.getElementById("password").value,
-    name: {
-      firstname: document.getElementById("firstname").value,
-      lastname: document.getElementById("lastname").value
-    },
-    address: {
-      city: document.getElementById("city").value,
-      street: document.getElementById("street").value,
-      number: document.getElementById("number").value,
-      zipcode: document.getElementById("zipcode").value,
-      geolocation: {
-        lat: document.getElementById("lat").value,
-        long: document.getElementById("long").value
+    fetch('https://fakestoreapi.com/users', {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
       }
-    },
-    phone: document.getElementById("phone").value
-  };
-  fetch('https://fakestoreapi.com/users', {
-    method: "POST",
-    body: JSON.stringify(data)
-  })
-    .then(res => res.json())
-    .then(json => { console.log(json); window.location.href = "login.html" });
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Registro exitoso:', data);
+      })
+      .catch(error => {
+        console.log('Error al registrar:', error);
+      });
+  });
 });
--- scriptProducts.js
+++ scriptProducts.js
@@ -8,28 +8,75 @@ class Product {
     }
 }
 
+const listaCarrito = document.querySelectorAll('.lista-carrito');
+const card = document.querySelector('.card');
 
 fetch('https://fakestoreapi.com/products')
     .then(res => res.json())
-    .then(json => {
-        const container = document.getElementById('container');
-        json.forEach(product => {
-            const card = document.createElement('div');
-            card.classList.add('card');
+    .then(data => {
+        data.forEach(producto => {
 
-            const title = document.createElement('h2');
-            title.textContent = product.title;
-
-            const price = document.createElement('p');
-            price.textContent = `Price: $${product.price}`;
-
-            const description = document.createElement('p');
-            description.textContent = product.description;
-
-            card.appendChild(title);
-            card.appendChild(price);
-            card.appendChild(description);
-
-            container.appendChild(card);
+            card.innerHTML += `
+            <div class="row">
+        <div class="info-card">
+          <h4>${producto.title}</h4>
+          <img src="${producto.image}" class="imagen-curso">
+          <p class="precio">${producto.price}$<span class="u-pull-right ">${producto.price}$</span></p>
+          <button type="button" class="btnAceptar" onclick="agregarCursoAlCarrito">Aceptar</button>
+        </div>
+        </div>`;
+            // Agregar la tarjeta al DOM
+            document.body.appendChild(card);
         });
     });
+function agregarCursoAlCarrito(e) {
+    e.preventDefault();
+    const curso = e.target.parentElement.parentElement;
+    obtenerDatosCurso(curso);
+}
+function obtenerDatosCurso(curso) {
+    const cursoInfo = {
+        imagen: curso.querySelector('.imagen-curso').src,
+        nombre: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('.agregar-carrito').getAttribute('data-id')
+    };
+    agregarCursoAlHTML(cursoInfo);
+}
+
+function agregarCursoAlHTML(curso) {
+    const row = document.createElement('tr');
+    console.log(curso);
+    row.innerHTML = `
+          <td>
+            <img src="${curso.imagen}" width="100">
+          </td>
+          <td>${curso.nombre}</td>
+          <td>${curso.precio}</td>
+          <td>
+            <input type="number" min="1" value="1">
+          </td>
+          <td>
+            <a class="borrar-curso" data-id="${curso.id}">X</a>
+          </td>
+        `;
+    listaCarrito.appendChild(row);
+}
+// const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+// vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+// listaCarrito.addEventListener('click', eliminarCurso)
+
+
+function vaciarCarrito() {
+    while (listaCarrito.firstChild) {
+        listaCarrito.removeChild(listaCarrito.firstChild);
+    }
+}
+
+function eliminarCurso(e) {
+    if (e.target.classList.contains('borrar-curso')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
