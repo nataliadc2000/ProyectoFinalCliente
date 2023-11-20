6bd6301623d2f55baac0c6dc9e65f26028551801
Natalia Domínguez <ndcarbajosa@gmail.com>
2023-11-20 18:30:58 +0100

    creacion de los carritos en buenas condiciones

--- scriptProducts.js
+++ scriptProducts.js
@@ -1,85 +1,156 @@
-class Product {
-    constructor(url, title, price, description, category) {
-        this.url = url;
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.category = category;
+//variables
+let allContainerCart = document.querySelector('.products');
+let containerBuyCart = document.querySelector('.card-items');
+let priceTotal = document.querySelector('.price-total')
+let amountProduct = document.querySelector('.count-product');
+
+
+const products = document.querySelector('.products');
+fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(json => {
+        json.forEach(element => {
+            console.log(element);
+            const newDiv = document.createElement("div");
+            newDiv.innerHTML = `
+                <div class="carts">
+                <div>
+                    <img src="${element.image}" alt="">
+                     <p><span>${element.price}</span>$</p>
+                 </div>
+                 <p class="title">${element.title}</p>
+                 <a href="" data-id="${element.id}" class="btn-add-cart">add to cart</a><br>
+                 <a href="" data-id="${element.id}" class="btn-add-cart">Editar cart</a>
+            </div>
+                `
+            localStorage.setItem(`newElement${element.id}`, `${element}${element.id}`)
+            products.appendChild(newDiv);
+        })
+    })
+
+
+let buyThings = [];
+let totalCard = 0;
+let countProduct = 0;
+
+//functions
+loadEventListenrs();
+function loadEventListenrs() {
+    allContainerCart.addEventListener('click', addProduct);
+
+    containerBuyCart.addEventListener('click', deleteProduct);
+}
+
+function addProduct(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('btn-add-cart')) {
+        const selectProduct = e.target.parentElement;
+        readTheContent(selectProduct);
     }
 }
 
-const listaCarrito = document.querySelectorAll('.lista-carrito');
-const card = document.querySelector('.columns');
+function deleteProduct(e) {
+    if (e.target.classList.contains('delete-product')) {
+        const deleteId = e.target.getAttribute('data-id');
 
-
-
-fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(producto => {
-
-            card.innerHTML += `
-            <div class="row">
-        <div class="info-card">
-          <h4>${producto.title}</h4>
-          <img src="${producto.image}" class="imagen-curso">
-          <p class="precio">${producto.price}$<span class="u-pull-right ">${producto.price}$</span></p>
-          <button type="button" class="btnAceptar" onclick="agregarCursoAlCarrito">Aceptar</button>
-        </div>
-        </div>`
+        buyThings.forEach(value => {
+            if (value.id == deleteId) {
+                let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
+                totalCard = totalCard - priceReduce;
+                totalCard = totalCard.toFixed(2);
+            }
         });
-        function agregarCursoAlCarrito(e) {
-            e.preventDefault();
-            const curso = e.target.parentElement.parentElement;
-            obtenerDatosCurso(curso);
-        }
-        function obtenerDatosCurso(curso) {
-            const cursoInfo = {
-                imagen: curso.querySelector('.imagen-curso').src,
-                nombre: curso.querySelector('h4').textContent,
-                precio: curso.querySelector('.precio span').textContent,
-                id: curso.querySelector('.agregar-carrito').getAttribute('data-id')
-            };
+        buyThings = buyThings.filter(product => product.id !== deleteId);
 
-            agregarCursoAlHTML(cursoInfo);
-        }
-        const carritoLista = document.getElementById('carrito-lista');
-        // carritoTotal = document.getElementById('carrito-total');
-        function agregarCursoAlHTML(curso) {
+        countProduct--;
+    }
+    //FIX: El contador se quedaba con "1" aunque ubiera 0 productos
+    if (buyThings.length === 0) {
+        priceTotal.innerHTML = 0;
+        amountProduct.innerHTML = 0;
+    }
+    loadHtml();
+}
 
-            var li = document.createElement('li');
-            li.innerText = curso.image + ' ' + curso.nombre + ' ' + curso.precio
+function readTheContent(product) {
+    const infoProduct = {
+        image: product.querySelector('div img').src,
+        title: product.querySelector('.title').textContent,
+        price: product.querySelector('div p span').textContent,
+        id: product.querySelector('a').getAttribute('data-id'),
+        amount: 1
+    }
 
-            // Agregar la tarjeta al DOM
-            carritoLista.appendChild(li);
-            //     row.innerHTML = `
-            //   <td>
-            //     <img src="${curso.imagen}" width="100">
-            //   </td>
-            //   <td>${curso.nombre}</td>
-            //   <td>${curso.precio}</td>
-            //   <td>
-            //     <input type="number" min="1" value="1">
-            //   </td>
-            //   <td>
-            //     <a class="borrar-curso" data-id="${curso.id}">X</a>
-            //   </td>
-            // // `;
-            // carritoLista.appendChild(row);
-        }
+    totalCard = parseFloat(totalCard) + parseFloat(infoProduct.price);
+    totalCard = totalCard.toFixed(2);
 
-
-
-
-        function vaciarCarrito() {
-            while (listaCarrito.firstChild) {
-                listaCarrito.removeChild(listaCarrito.firstChild);
+    const exist = buyThings.some(product => product.id === infoProduct.id);
+    if (exist) {
+        const pro = buyThings.map(product => {
+            if (product.id === infoProduct.id) {
+                product.amount++;
+                return product;
+            } else {
+                return product
             }
-        }
+        });
+        buyThings = [...pro];
+    } else {
+        buyThings = [...buyThings, infoProduct]
+        countProduct++;
+    }
+    loadHtml();
+    //console.log(infoProduct);
+}
 
-        function eliminarCurso(e) {
-            if (e.target.classList.contains('borrar-curso')) {
-                e.target.parentElement.parentElement.remove();
+function loadHtml() {
+    clearHtml();
+    buyThings.forEach(product => {
+        const { image, title, price, amount, id } = product;
+        const row = document.createElement('div');
+        row.classList.add('item');
+        row.innerHTML = `
+            <img src="${image}" alt="">
+            <div class="item-content">
+                <h5>${title}</h5>
+                <h5 class="cart-price">${price}$</h5>
+                <h6>Amount: ${amount}</h6>
+            </div>
+            <span class="delete-product" data-id="${id}">X</span>
+        `;
+
+        containerBuyCart.appendChild(row);
+
+        priceTotal.innerHTML = totalCard;
+
+        amountProduct.innerHTML = countProduct;
+    });
+}
+function clearHtml() {
+    containerBuyCart.innerHTML = '';
+}
+
+function agregarproducto() {
+
+    const name = document.querySelector(".name");
+    const price = document.querySelector(".price");
+    const description = document.querySelector(".description");
+    const imagen = document.querySelector(".image");
+    const category = document.querySelector(".categoria");
+    fetch('https://fakestoreapi.com/products', {
+        method: "POST",
+        body: JSON.stringify(
+            {
+                title: name.value,
+                price: price.value,
+                description: description.value,
+                image: imagen.value,
+                category: category.value
             }
-        }
+        )
     })
+        .then(res => res.json())
+        .then(json => {
+            localStorage.setItem('nuevoElemento', json);
+        })
+}
